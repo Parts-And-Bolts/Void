8a580a422c9829a49b83405d2b5eb67b4dc74f04
Parts-And-Bolts <danial1310201@gmail.com>
2024-09-22 19:14:07 +0800

    r
    r

--- Scripts/interactable_object_2d.gd
+++ Scripts/interactable_object_2d.gd
@@ -6,13 +6,13 @@ func execute():
 	if isInside and not isPressed:
 		var object = get_node(objectPath)
 		isPressed = true
-		print(object.name)
+		
 		object.position = Vector2(-500,500)
 
 func _on_body_entered(body: Node2D) -> void:
 	
 	if body is Player:
-		print("yo")
+		
 		body.currentInteractable = self
 		isInside = true
 
@@ -20,5 +20,6 @@ func _on_body_entered(body: Node2D) -> void:
 func _on_body_exited(body: Node2D) -> void:
 	
 	if body is Player:
+		
 		body.currentInteractable = null
 		isInside = false
